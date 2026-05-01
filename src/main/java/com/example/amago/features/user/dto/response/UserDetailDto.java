f518bca581bbfd362042dce34b6f5035c9127834
lazaroalexadre <lazaroalexandre2022@gmail.com>
2026-05-01 17:54:10 -0300

    fix #11: corrigindo dados do dto de responde de usuário

--- src/main/java/com/example/amago/features/user/dto/response/UserDetailDto.java
+++ src/main/java/com/example/amago/features/user/dto/response/UserDetailDto.java
@@ -2,6 +2,6 @@ package com.example.amago.features.user.dto.response;
 
 import java.time.LocalDateTime;
 
-public record UserDetailDto(String id, String name, String email, String bio, String image, LocalDateTime created,
-        LocalDateTime updated) {
+public record UserDetailDto(String id, String name, String email, String bio, String image, LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
 }
